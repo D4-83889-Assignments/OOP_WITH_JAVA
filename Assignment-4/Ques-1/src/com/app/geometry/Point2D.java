feb98e2f1381628df249fc6314e4e9e07a99da9d
sunbeam-pdac <onlyvm10>
2024-04-24 07:46:16 -0700

    msg

---
+++ Assignment-4/Ques-1/src/com/app/geometry/Point2D.java
@@ -0,0 +1,36 @@
+package com.app.geometry;
+
+public class Point2D {
+	public int xCoord;
+	public int yCoord;
+	
+	public Point2D(int xCoord, int yCoord) {
+	
+		this.xCoord = xCoord;
+		this.yCoord = yCoord;
+	}
+	
+
+	public String getDetails() {
+		return "x-coordinate: "+String.valueOf(xCoord)+" y-coordinate: "+String.valueOf(yCoord);
+	}
+	
+	public boolean isEqual(Point2D p) {
+		
+		if(this.yCoord==p.yCoord && this.xCoord==p.xCoord)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	public double calculateDistance(Point2D p) {
+		return Math.sqrt(Math.pow((this.xCoord-p.xCoord),2)+Math.pow((this.yCoord-p.yCoord),2));
+	}
+
+	
+	
+}
---
+++ Assignment-4/Ques-1/src/tester/TestPointArray1.java
@@ -0,0 +1,109 @@
+package tester;
+
+import java.awt.SystemColor;
+import java.util.Iterator;
+import java.util.Scanner;
+
+import com.app.geometry.Point2D;
+
+
+public class TestPointArray1 {
+
+	public static int menu() {
+		
+		int choice;
+		System.out.println("*****************Welcome to Application*********************");
+		System.out.println("1.Display Details of a specific point");
+		System.out.println("2.Display x,y coordinates of all points");
+		System.out.println("3.Calculate Distance between two points");
+		System.out.println("4.Exit");
+		System.out.println("Enter your choice");
+		choice  = new Scanner(System.in).nextInt();
+		
+		
+		
+		return choice;
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter no. of point you want to Create....");
+		Point2D [] arr  = new Point2D[new Scanner(System.in).nextInt()];
+		int x,y;
+
+		
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println("Enter X Coordinate");
+			x = new Scanner(System.in).nextInt();
+			
+			System.out.println("Enter Y Coordinate");
+			y = new Scanner(System.in).nextInt();
+			
+			arr[i] = new Point2D(x,y);
+//			System.out.println();
+		}
+		
+		int choice;
+		
+		while((choice=menu())!=4)
+		{
+			switch (choice) {
+			case 1:
+				System.out.println("Enter index of a point you want to check..");
+				int index = new Scanner(System.in).nextInt();
+				if(index>=0 && index<=arr.length-1)
+					System.out.println(arr[index].getDetails());
+				else
+					System.out.println("Enter valid index..");
+				break;
+				
+			case 2:
+				for (int i = 0; i < arr.length; i++) {
+					System.out.println("X-coord: "+arr[i].xCoord);
+					System.out.println("Y-coord: "+arr[i].yCoord);
+				}
+				
+				break;
+			case 3:
+				System.out.println("Enter start point index");
+				int Sindex  = new Scanner(System.in).nextInt();
+				
+				if(Sindex>=0)
+				{
+					System.out.println("Enter end point index");
+					int Eindex = new Scanner(System.in).nextInt();
+					if(Eindex<=arr.length-1)
+					{
+						System.out.println("Distance between the point: "+arr[Sindex].calculateDistance(arr[Eindex]));
+						
+					}
+					else
+					{
+						System.out.println("End index is out of bounds...");
+					}
+				}
+				else
+				{
+					System.out.println("Start index should be greater than or equal to zero...");
+				}
+			
+				
+
+				
+				
+				
+				
+				break;
+			default:
+				System.out.println("Wrong Choice Given....Try Again");
+				break;
+			}
+		}
+		System.out.println("Thanks for using my Application");
+	}
+	
+
+}
+
+
